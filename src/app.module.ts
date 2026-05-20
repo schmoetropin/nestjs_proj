52e8cee3ca678da0690af69f4fe05ba362f06023
schmoetropin <schmoemaster@gmail.com>
2026-05-20 13:26:08 -0300

    feat: typeorm and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,12 @@ import { TaskModule } from './task/task.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}), 
-    TaskModule, UsersModule, AuthModule
+    TaskModule, UsersModule, AuthModule, DbModule
   ],
   controllers: [AppController],
   providers: [AppService],
